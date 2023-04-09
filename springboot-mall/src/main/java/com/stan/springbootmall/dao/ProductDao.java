2a50e3a7177a2ad9f715d48ac9f8ad28ffad40a1
mac <vision158756@gmail.com>
2023-04-09 17:01:30 +0800

    商品功能 - 實作查詢商品列表(基礎實作)

--- springboot-mall/src/main/java/com/stan/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/stan/springbootmall/dao/ProductDao.java
@@ -3,7 +3,12 @@ package com.stan.springbootmall.dao;
 import com.stan.springbootmall.dto.ProductRequest;
 import com.stan.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- springboot-mall/src/main/java/com/stan/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/stan/springbootmall/service/ProductService.java
@@ -3,7 +3,14 @@ package com.stan.springbootmall.service;
 import com.stan.springbootmall.dto.ProductRequest;
 import com.stan.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+
+    List<Product> getProducts();
+
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
